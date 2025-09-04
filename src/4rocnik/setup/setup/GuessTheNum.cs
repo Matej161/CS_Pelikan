56c87bb1372d13bd12daaf5c17ad3f305ec67f1b
Matěj Kulíšek <matej.kulisek@student.spsmb.cz>
2025-09-04 11:20:05 +0200

    first

---
+++ src/4rocnik/setup/setup/GuessTheNum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace setup
+{
+    public class GuessTheNum
+    {
+        public int min;
+        public int max;
+
+        public GuessTheNum(int min, int max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+
+        public int RandomNum()
+        {
+            Random random = new Random();
+            int num = random.Next(min, max);
+            return num;
+        }
+    }
+    
+}
--- src/4rocnik/setup/setup/Program.cs
+++ src/4rocnik/setup/setup/Program.cs
@@ -1,9 +1,38 @@
+using System;
+
 namespace setup
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            GuessTheNum guessTheNum = new GuessTheNum(1, 5);
+            for (int i = 0; i <= 100; i++)
+            {
+                FizzBuzz(i);
+            }
+        }
+
+        public static void FizzBuzz(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                Console.WriteLine("fizzbuzz");
+            }
+            else if (num % 3 == 0)
+            {
+                Console.WriteLine("fizz");
+                
+            }
+            else if (num % 5 == 0)
+            {
+                Console.WriteLine("buzz");
+                
+            }
+            else
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
