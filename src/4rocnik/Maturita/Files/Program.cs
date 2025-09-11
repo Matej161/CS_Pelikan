9a234c907089ec4e4a8612ab9f4778ef94902fb8
Matěj Kulíšek <matej.kulisek@student.spsmb.cz>
2025-09-11 11:19:06 +0200

    first

--- src/4rocnik/Maturita/Files/Program.cs
+++ src/4rocnik/Maturita/Files/Program.cs
@@ -1,9 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Files.Properties;
+
 namespace Files
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static List<Movies> Movies = new List<Movies>();
+
+        public static void Main(string[] args)
+        {
+            GetMovie("./movies.csv");
+            Console.WriteLine(Movies);
+        }
+
+        public static void GetMovie(string filePath)
+        {
+            using (StreamReader scvReader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = scvReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+
+                var parts = line.Split(',');
+                
+                Movies.Add(new Movies(parts[0], parts[1], parts[2], parts[3],  parts[4],  parts[5], parts[6]));
+            }
+        }
     }
-  }
 }
---
+++ src/4rocnik/Maturita/Files/Properties/Movies.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Files.Properties
+{
+    public class Movies
+    {
+        private string film;
+        private string genre;
+        private string leadStudio;
+        private string audienceScore;
+        private string profit;
+        private string rottenTomatoes;
+        private string year;
+
+        public Movies(string film, string genre, string leadStudio, string audienceScore, string profit,
+            string rottenTomatoes,
+            string year)
+        {
+            this.film = film;
+            this.genre = genre;
+            this.leadStudio = leadStudio;
+            this.audienceScore = audienceScore;
+            this.profit = profit;
+            this.rottenTomatoes = rottenTomatoes;
+            this.year = year;
+        }
+
+        public override string ToString()
+        {
+            return film;
+        }
+    }
+}
